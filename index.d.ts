0669fbbb20875a5a0ab6d9f6de454f2d0cf29cde
spadesk1991@gmail.com <spadesk1991@gmail.com>
2019-12-13 18:09:41 +0800

    fix: remove hh field

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,6 @@ import * as  Raven from 'raven';
   declare module 'egg' {
     export interface Application {
       raven: Raven
-      hh :String
     }
      // extend your config
     export interface EggAppConfig {
